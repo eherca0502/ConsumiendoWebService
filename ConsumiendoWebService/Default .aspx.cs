92fc35f4f77b754bd2dd1d053aa217ea1d2d6f54
eduar <eduar@DESKTOP-CQ4EC7K>
2023-11-06 13:47:32 -0600

    Agregar archivos de proyecto.

---
+++ ConsumiendoWebService/Default .aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumiendoWebService
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnMostrar_Click(object sender, EventArgs e)
+        {
+            NorthWS.ConsultaNorthwind obj = new NorthWS.ConsultaNorthwind();
+            GridView1.DataSource = obj.GetData();
+            GridView1.DataBind();
+        }
+    }
+}
